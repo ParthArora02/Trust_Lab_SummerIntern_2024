f0bf7c65d2503b5541dc0552525b8526e83e49b9
ParthArora02 <99160857+ParthArora02@users.noreply.github.com>
2024-07-10 21:00:25 +0530

    Update gas_fees.py

--- gas_fees.py
+++ gas_fees.py
@@ -4,7 +4,9 @@ def get_gas_fees(api_key):
     url = f"https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={api_key}" # use your api key for getting the prices from etherscan
     response = requests.get(url)
     data = response.json()
-
+# there are 3 types of gas prices i.e. safe, proposed and fast https://etherscan.io/gastracker
+# safe is cheapest but may take longer times for the transactions, proposed is the recommended one for general
+# purposes, fast is expensive than both of the previous ones, but makes fast transactions.
     if data['status'] == '1':  # Successful response
         safe_gas_price = data['result']['SafeGasPrice']
         propose_gas_price = data['result']['ProposeGasPrice']
@@ -16,4 +18,4 @@ def get_gas_fees(api_key):
             "FastGasPrice": fast_gas_price
         }
     else:
-        raise Exception("Error fetching gas fees from Etherscan")
+        raise Exception("Error fetching gas fees from Etherscan") # throws error if it is unable to get the data
