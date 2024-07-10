39dc88c14041748b2b3dbfb0e127dad6f8a54d31
ParthArora02 <99160857+ParthArora02@users.noreply.github.com>
2024-07-10 20:54:38 +0530

    Update network.py

--- network.py
+++ network.py
@@ -2,7 +2,7 @@ import random
 import csv
 
 from constants import MIN_LINKS, MAX_LINKS
-
+# min and max number of nodes
 def filter_out_city(adjacency_list):
     to_del = []
     for source, _ in adjacency_list.items():
@@ -12,7 +12,7 @@ def filter_out_city(adjacency_list):
     for id in to_del:
         del adjacency_list[id]
 
-def inter_city_latency(file_path):
+def inter_city_latency(file_path):#function for fetching the inter city data from the dataset  https://wp-public.s3.amazonaws.com/pings/pings-2020-07-19-2020-07-20.csv.gz
     adjacency_list = {}
 
     with open(file_path, 'r') as csvfile:
@@ -29,7 +29,7 @@ def inter_city_latency(file_path):
     
     return adjacency_list
 
-def get_server_data(file_path):
+def get_server_data(file_path): #getting server data from the dataset 
     servers = {}
 
     with open(file_path, 'r') as csvfile:
@@ -42,11 +42,11 @@ def get_server_data(file_path):
     
     return servers
 
-def create_network(network, next_elms, que, city_latency):
+def create_network(network, next_elms, que, city_latency): # creating a connected network with the use of graph data structure, uses queue for the immplementation
     visited = [False for _ in range(len(network))]
     
     while que:
-        links = random.randint(MIN_LINKS, MAX_LINKS)
+        links = random.randint(MIN_LINKS, MAX_LINKS) #standard algorithm followed for creating graph 
         next_elms = random.sample(next_elms, len(next_elms))
         u = que.pop(0)
         visited[u] = True
@@ -90,24 +90,24 @@ def create_network(network, next_elms, que, city_latency):
                 visited[neighbor_id] = True
         # print(f"peer {u} connections: {len(network[u].neighbours)}")
 
-def dfs(u, network, visited):
+def dfs(u, network, visited): # depth first search for graph traversal for checking the connections made in the network
     visited[u] = True
     for elm in network[u].neighbours:
         if not visited[elm]:
             dfs(elm, network, visited)
 
-def is_connected(network):
+def is_connected(network):# checks whether all the nodes are connected in the graph and returns true if all are connected
     visited = [False for _ in range(len(network))]
     dfs(0, network, visited)
     connected = all(visited)
     print(f"Network connected: {connected}")
     return connected
 
-def reset_network(network):
+def reset_network(network):# this function would be called to delete the network, would be called if all the nodes aren't connected and then the process of the network creation starts again 
     for i in range(len(network)):
         network[i].neighbours.clear()
 
-def check_links(network):
+def check_links(network): # checks that every node in the network follows the criteria of min_links and max_links and returns false if not followed
     for i in range(1, len(network)):
         if len(network[i].neighbours) < MIN_LINKS:
             print(f"peer {i} has less than {MIN_LINKS} connections")
@@ -115,13 +115,13 @@ def check_links(network):
     print(f"All peers have at least {MIN_LINKS} connections")
     return True
 
-def print_network(network):
+def print_network(network):# when the network is created finally, it would pring all the network
     for peer in network:
         print(f"peer {peer.id} (Slow={peer.is_slow}): {peer.neighbours}")
         # print(f"peer {peer.id} ({peer.bandwidth:.2f} Mbps): {peer.neighbours}")
 
-def finalise_network(n, network, city_latency):
-    attempt = 0
+def finalise_network(n, network, city_latency):# trying multiple times to create the network, the number of attempts could be adjusted accordingly to the number of nodes required in the network
+    attempt = 0 # for 100 nodes , everytime the network was created in less than 80 attempts , so on a safer side took 100 iterations
     while attempt < 100:
         attempt += 1
         print(f"Attempt {attempt}: Creating network")
